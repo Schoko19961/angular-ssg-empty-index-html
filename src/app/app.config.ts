07d3005b2a90e16399e16c7f27b8c4d74ea4a2a4
David Wennemaring <20268159+Schoko19961@users.noreply.github.com>
2025-11-25 15:04:20 +0100

    Narrow down even further

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,14 @@
 import {
   ApplicationConfig,
   CSP_NONCE,
-  ErrorHandler,
   inject,
-  NgZone,
   provideBrowserGlobalErrorListeners,
   provideEnvironmentInitializer,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ActivatedRoute, provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { CustomErrorHandler } from './custom-error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,11 +17,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideEnvironmentInitializer(() => {
+      // const activatedRoute = inject(ActivatedRoute)
       const csp_nonce = inject(CSP_NONCE);
     }),
-    {
-      provide: ErrorHandler,
-      useClass: CustomErrorHandler,
-    },
   ],
 };
--- src/app/custom-error-handler.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorHandler, inject, Injectable } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-@Injectable({ providedIn: 'root' })
-export class CustomErrorHandler extends ErrorHandler implements ErrorHandler {
-    // activatedRoute = inject(ActivatedRoute)
-  override handleError(error: Error): void {
-    super.handleError(error);
-  }
-}
